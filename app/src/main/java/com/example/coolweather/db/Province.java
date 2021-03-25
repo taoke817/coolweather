9f17815adb72d771085f74ec05bd4c9e31ca3664
tinker321 <tk489352855@qq.com>
2021-03-25 10:37:15 +0800

    DB&Config

---
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.coolweather.db;
+
+/**
+ * Created by HUWEI on 2021/03/25.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+
+
+}
